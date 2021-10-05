91fc490f837147c1d7441074e077e1f16f81f525
Andrés Geovanny Angulo Botina <andrescabj981@gmail.com>
2021-10-04 22:56:31 -0500

    v1.0 - Updated component ResourceActivity and Added status on activities of subtopics on thematics asset - 04-10-2021

--- src/components/Common/ResourceActivity/ResourceActivity.js
+++ src/components/Common/ResourceActivity/ResourceActivity.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const ResourceActivity = ({activity}) => {
 
+    if (activity.status) {
+        
+        return <iframe className="w-full" loading="lazy" src={activity.url} height="720"></iframe>
 
-    return <iframe className="w-full" loading="lazy" src={activity.url} height="720"></iframe>
+    } else {
+
+        return <h3 className="font-normal text-md italic text-red-600">Aún no hay actividades disponibles</h3>;
+
+    }
+
 
 }
 
