887b1a75640a70905ae185e9f001d22038708144
Andrés Geovanny Angulo Botina <andrescabj981@gmail.com>
2021-11-03 15:04:54 -0500

    v1.1 - Some images added to examples of each thematic, image labels updated to load resources without node server, SubtopicImage updated to show all content of loaded image - 03-11-2021

--- src/components/Common/Creators/Creator.js
+++ src/components/Common/Creators/Creator.js
@@ -7,7 +7,8 @@ class Creator extends React.Component {
         return (
 
             <div className="flex flex-col items-center justify-center">
-                <img src={process.env.PUBLIC_URL + require('./../../../assets/img/everyone/' + this.props.creator.avatar).default} className="w-20 h-20 object-cover rounded-full" alt="creator.name" />
+                <img src={require('./../../../assets/img/everyone/' + this.props.creator.avatar).default} className="w-20 h-20 object-cover rounded-full" alt="creator.name" />
+                {/*<img src={process.env.PUBLIC_URL + require('./../../../assets/img/everyone/' + this.props.creator.avatar).default} className="w-20 h-20 object-cover rounded-full" alt="creator.name" />*/}
                 <h3 className="font-semibold text-md pt-4">{ this.props.creator.name }</h3>
                 <div className="flex flex-col items-center space-y-0">
                     <span className="font-normal text-xs">{ this.props.creator.role }</span>
--- src/components/Common/SubtopicImage/SubtopicImage.js
+++ src/components/Common/SubtopicImage/SubtopicImage.js
@@ -8,9 +8,9 @@ const SubtopicImage = ({resource, show}) => {
 
             <div className="w-full h-full flex flex-col lg:flex-row">
 
-                <img className="object-cover w-full lg:w-1/2 rounded-t-md lg:rounded-l-md shadow-md" src={process.env.PUBLIC_URL + require('./../../../assets/img/material/' + resource.resource_path).default} alt="" />
+                <img className="bg-contain bg-center w-full lg:w-2/3 rounded-t-md lg:rounded-l-md shadow-md" src={require('./../../../assets/img/material/' + resource.resource_path).default} alt="" />
 
-                <div className="m-0 md:m-auto w-full lg:w-1/2 p-4 md:p-16">
+                <div className="m-0 md:m-auto w-full lg:w-1/3 p-4 md:p-16">
                     <h3 className="font-semibold text-2xl lg:text-2xl 2xl:text-3xl text-black">{resource.name}</h3>
                     <p className="font-normal text-md sm:text-lg lg:text-md 2xl:text-xl text-justify">{resource.description}</p>
                 </div>
